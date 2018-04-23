f078b174c9aa4cdbbe935824a37ad75a05ac5568
industryfatcat <36923870+industryfatcat@users.noreply.github.com>
2018-04-23 09:53:11 -0400

    Update nutUtils.js

--- nutUtils.js
+++ nutUtils.js
@@ -1,5 +1,6 @@
 function nutFunc()
 {
-  document.getElementById("welcome").innerHTML="Nutting."
-  document.getElementById("activate").style.display="none"
+  document.getElementById("welcome").innerHTML="Nutting.";
+  document.getElementById("activate").style.display="none";
+  document.getElementById("totodile").style.display="block";
 }
