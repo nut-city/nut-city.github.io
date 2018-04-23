dbb616df7b7e9cad98c9fe00da85fd0cff009165
industryfatcat <36923870+industryfatcat@users.noreply.github.com>
2018-04-23 09:57:14 -0400

    added nutStop

--- nutUtils.js
+++ nutUtils.js
@@ -1,6 +1,13 @@
 function nutFunc()
 {
   document.getElementById("welcome").innerHTML="Nutting.";
-  document.getElementById("activate").style.display="none";
+  document.getElementById("activate").innerHTML="Done?";
   document.getElementById("totodile").style.display="block";
+}
+
+function nutStop()
+{
+  document.getElementById("welcome").innerHTML="Thank you for visiting Nut City!";
+  document.getElementById("totodile").style.display="none";
+  document.getElementById("activate").style.display="none";
 }
