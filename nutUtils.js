df4aeedcdcb1e604905884f528d27d2ea1028fff
hunter <industryfatcat@gmail.com>
2018-04-23 16:12:25 -0400

    nutStop enabled

--- nutUtils.js
+++ nutUtils.js
@@ -1,6 +1,14 @@
 function nutFunc()
 {
-  document.getElementById("welcome").innerHTML="Nutting.";
-  document.getElementById("activate").style.display="none";
-  document.getElementById("totodile").style.display="block";
+  document.getElementById("welcome").innerHTML = "Nutting.";
+  document.getElementById("activate").innerHTML = "Click to stop nutting";
+  document.getElementById("activate").onclick = nutStop;
+  document.getElementById("totodile").style.display = "block";
+}
+
+function nutStop()
+{
+  document.getElementById("welcome").innerHTML = "Thanks for visiting Nut City!!";
+  document.getElementById("activate").style.display = "none";
+  document.getElementById("totodile").style.display = "none";
 }
